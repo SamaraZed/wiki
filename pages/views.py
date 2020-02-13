42604ac8f405eee3900a3f411296350593387cfe
Ayham Samara <Ayham.Samara@live.com>
2020-02-13 13:25:13 +0200

    Working code with test result 'OK'

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from .models import Page
 # Create your views here.
+def pages_list(request):
+    context = {
+        "pages": Page.objects.all()
+    }
+    return render(request, 'list.html', context)
+def pages_detail(request, page_id):
+    context = {
+        "page": Page.objects.get(id=page_id)
+    }
+    return render(request, 'detail.html', context)
